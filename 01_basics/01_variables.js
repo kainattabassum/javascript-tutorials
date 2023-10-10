0d1aeeff92e4e4f6b4527e34f2baefb7a59d4920
Kainat Tabassum <53490506+kainattabassum@users.noreply.github.com>
2023-10-10 12:31:34 +0000

    Day 1

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 456524
+let accountEmail = "kainat.tabasum001@gmail.com"
+var accountPassword = "123456"
+accountCity = "Lahore"
+let accountState
+
+// accountId = 287 // NOT ALLOWED
+accountEmail = "changes@gmail.com"
+accountPassword = "565678"
+accountCity = "Karachi"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("name")
+
+let name = "name"
+let age = 27
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
