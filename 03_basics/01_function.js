96282acad181690b8ea78863c163240cef8fe95a
Kainat Tabassum <53490506+kainattabassum@users.noreply.github.com>
2023-10-17 17:43:59 +0000

    Day 5 - functions

---
+++ 03_basics/01_function.js
@@ -0,0 +1,12 @@
+function sayMyName() {
+    console.log('kainat');
+    console.log('tabassum');
+}
+
+// sayMyName()
+
+function addNumbers(num1, num2) {
+    console.log(num1+num2);
+}
+
+addNumbers(4, 6)
