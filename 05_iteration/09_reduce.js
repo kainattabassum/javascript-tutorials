7f42bf7d81c53b7285d015844a8a6348a6a2587a
Kainat Tabassum <53490506+kainattabassum@users.noreply.github.com>
2023-10-26 11:02:50 +0000

    Day 10 - iteration

---
+++ 05_iteration/09_reduce.js
@@ -0,0 +1,34 @@
+const myNums = [1, 2, 3, 4];
+
+// reduce((accumlator, currentVal) => {}, initialValue)
+const initialValue = 0;
+const myTotal = myNums.reduce((accumlator, currentVal) => {
+  // console.log(`accumlator: ${accumlator} and current value: ${currentVal}`);
+  return accumlator + currentVal;
+}, initialValue);
+// console.log(myTotal);
+
+const anotherWay = myNums.reduce((acc, cur) => acc + cur, 0)
+// console.log(anotherWay);
+
+const shoppingCart = [
+  {
+    itemName: "js course",
+    price: 2999,
+  },
+  {
+    itemName: "py course",
+    price: 999,
+  },
+  {
+    itemName: "mobile dev course",
+    price: 5999,
+  },
+  {
+    itemName: "data science course",
+    price: 12999,
+  },
+];
+
+const totalCost = shoppingCart.reduce((accumlator, item) => accumlator + item.price, 0)
+console.log(totalCost);
