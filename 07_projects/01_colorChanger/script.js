8efbf09d84e47fc33ea79cb939b6c398219ee3c3
Kainat Tabassum <53490506+kainattabassum@users.noreply.github.com>
2023-11-08 13:40:08 +0000

    Day 14 - project 1

--- 07_projects/01_colorChanger/script.js
+++ 07_projects/01_colorChanger/script.js
@@ -1,9 +1,28 @@
-const buttons = document.querySelectorAll(".button")
-const body = document.querySelector('body')
+const buttons = document.querySelectorAll(".button");
+const body = document.querySelector("body");
 
-buttons.forEach(function(button) {
-    // console.log(button);
-    button.addEventListener("onclick", function() {
-        LockManager
-    })
-})
+buttons.forEach(function (button) {
+  // console.log(button);
+  button.addEventListener("click", function (e) {
+    // console.log(e);
+    // console.log(e.target);
+
+    switch (e.target.id) {
+      case "grey":
+        body.style.background = e.target.id;
+        break;
+      case "white":
+        body.style.background = e.target.id;
+        break;
+      case "blue":
+        body.style.background = e.target.id;
+        break;
+      case "yellow":
+        body.style.background = e.target.id;
+        break;
+      default:
+        body.style.background = "skyblue";
+        break;
+    }
+  });
+});
