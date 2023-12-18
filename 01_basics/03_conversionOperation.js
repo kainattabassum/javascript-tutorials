80ce371addaf363f1c222ba0aa66a27d9c7013d1
Kainat Tabassum <53490506+kainattabassum@users.noreply.github.com>
2023-12-18 13:07:23 +0000

    log

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -8,28 +8,28 @@ let valueInNumber = Number(score);
 
 // "33" => 33
 // "33abc" => NaN
-//  true => 1; false => 0
+// true => 1; false => 0
 // null => 0
 // undefined => NaN
 
 let isLoggedIn = undefined;
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-// console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn); // false
 
 // 1 => true; 0 => false
 // "" => false
-// "kianat" => true
+// "kainat" => true
 
 let someNumber = 33;
 let stringNumber = String(someNumber);
 // console.log(stringNumber);
-// console.log(typeof stringNumber);
+// console.log(typeof stringNumber); // string
 
 // *********************** Operations ***********************
 
 let value = 3;
 let negValue = -value;
-// console.log(negValue);
+// console.log(negValue); // -3
 
 // console.log(2+2);
 // console.log(2-2);
@@ -41,21 +41,21 @@ let negValue = -value;
 let str1 = "hellow";
 let str2 = " kai";
 let str3 = str1 + str2;
-// console.log(str3);
+// console.log(str3); // hellow kai
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); // 32
 
-// console.log( (3 + 4) * 5 % 3);
+// console.log( (3 + 4) * 5 % 3); // 2
 
-// console.log(+true); // not
-// console.log(+""); // not
+// console.log(+true); // not - 1
+// console.log(+""); // not - 2
 
 let num1, num2, num3;
 num1 = num2 = num3 = 2 + 2;
-// console.table([num1, num2, num3])
+// console.table([num1, num2, num3]) // 4 all
 
 let gameCounter = 100;
 ++gameCounter;
--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -2,19 +2,21 @@ const name = "usernameOne";
 const repoCount = 3;
 
 // string interpolation ``
-console.log(`my name is ${name} and repo count is ${repoCount}.`);
+// console.log(`my name is ${name} and repo count is ${repoCount}.`);
 
 const gameName = new String("username-one");
 
-// console.log(gameName);
-// console.log(gameName[0]);
-// console.log(gameName.length);
-// console.log(gameName.__proto__);
-// console.log(typeof gameName); // Object
+console.log(gameName);
+console.log(gameName[0]);
+console.log(gameName.length);
+console.log(gameName.__proto__);
+console.log(typeof gameName); // Object
 
 // console.log(gameName.toUpperCase());
 // console.log(gameName.charAt(2));
 // console.log(gameName.indexOf('a'));
+
+console.log();
 
 const newString = gameName.substring(4, 9);
 // console.log(newString);
