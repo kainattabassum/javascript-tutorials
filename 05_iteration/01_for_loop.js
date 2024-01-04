3e7b65d230842e0615b07b459c6e5c7765952675
Kainat Tabassum <53490506+kainattabassum@users.noreply.github.com>
2024-01-04 12:01:21 +0000

    review - 05_iteration Day 3

--- 05_iteration/01_for_loop.js
+++ 05_iteration/01_for_loop.js
@@ -1,19 +1,18 @@
-// for
-
+// for loop
 for (let index = 0; index <= 10; index++) {
   const element = index;
   if (element == 5) {
-    // console.log("5 is best number");
+    // console.log(`${index} is best number`);
   }
   // console.log(element);
 }
 
-for (let i = 1; i <= 10; i++) {
-  // console.log(`Outer Loop: ${i}`);
-  for (let j = 1; j <= 10; j++) {
+for (let i = 1; i <= 5; i++) {
+  console.log(`Outer Loop: ${i}`);
+  for (let j = 1; j <= 5; j++) {
     // console.log(`j Inner Loop: ${j}, i Inner Loop: ${i}`);
     // console.log(`| ${i} | ${j} | ${i} - ${j} |`);
-    // console.log(`${i} * ${j} = ${i * j}`);
+    console.log(`${i} * ${j} = ${i * j}`);
   }
 }
 
@@ -25,18 +24,18 @@ for (let index = 0; index < myArr.length; index++) {
 }
 
 // ++++++++++++++++++ break and continue
-// for (let index = 1; index <= 5; index++) {
-//   if (index == 2) {
-//     console.log(`Detected 2`);
-//     break;
-//   }
-//   console.log(`Value of i is ${index}`);
-// }
+for (let index = 1; index <= 5; index++) {
+  if (index == 2) {
+    // console.log(`Detected 2`);
+    break;
+  }
+  // console.log(`Value of i is ${index}`);
+}
 
 for (let index = 1; index <= 5; index++) {
   if (index == 2) {
-    console.log(`Detected 2`);
+    // console.log(`Detected 2`);
     continue;
   }
-  console.log(`Value of i is ${index}`);
+  // console.log(`Value of i is ${index}`);
 }
